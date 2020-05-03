7605481f2d2bc4e343a40e87a10bab8f7bb8dc74
MortenBE <Ehlersmorten@gmail.com>
2020-05-03 19:26:30 +0200

    views

---
+++ TheSocialNetwork/Queries/Views.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheSocialNetwork.Models;
+using TheSocialNetwork.Services;
+
+namespace TheSocialNetwork.Queries
+{
+    class Views
+    {
+        private readonly PostService _postService;
+        private readonly UserService _userService;
+
+        public void ShowUserPost(User user)
+        {
+
+        }
+
+    }
+}
