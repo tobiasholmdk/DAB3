231103996f142aea2dc503ddede8656e9b999c9e
Frederik Runge-Dalager <Frederik Runge-Dalag@LAPTOP-FC9QT90T>
2020-05-03 12:49:14 +0200

    begyndt på post services

---
+++ TheSocialNetwork/Services/PostService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Driver;
+using TheSocialNetwork.Models;
+namespace TheSocialNetwork.Services
+{
+    class PostService
+    {
+        private readonly IMongoCollection<Post> _posts;
+
+        public PostService()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("TheSocialNetworkDatabase");
+
+            _posts = database.GetCollection<Post>("Posts");
+        }
+
+        public List<Post> Get() =>
+            _posts.Find(p => true).ToList();
+
+        public Post GetById(int id)
+        {
+            return _posts.Find(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Post GetbyUser(User user)
+        {
+            return _posts.Find(p => p.Author == user.Id).ToList();
+        }
+
+
+    }
+}
